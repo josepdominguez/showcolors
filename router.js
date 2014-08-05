c90907d93bef58ac8036093f6cdd4142d50ac14a
Josep Dominguez <jd@goldengekko.com>
2014-08-05 12:04:12 +0200

    All

---
+++ router.js
@@ -0,0 +1,5 @@
+function route(pathname) {  
+    //console.log("test");
+}
+
+exports.route = route;
---
+++ start.js
@@ -0,0 +1,7 @@
+var mongoDbConnection = require('connection.js');
+
+function start(route) {
+    console.log("tgest");
+}
+
+exports.start = start;
